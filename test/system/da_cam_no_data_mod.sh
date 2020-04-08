972c89699a09bc5ae2c936f94aff765892e107c4
Steve Goldhaber <goldy@ucar.edu>
2020-04-08 10:47:49 -0600

    Fix CAM DAE test to update log message check and use updated initial files as input

--- test/system/da_cam_no_data_mod.sh
+++ test/system/da_cam_no_data_mod.sh
@@ -43,7 +43,7 @@ else
         if [ $ninst -eq 1 ]; then
           lfiles="`ls -t atm.log.* 2> /dev/null | head -1`"
         else
-          # Multi-instance, look for wav_nnnn.log*
+          # Multi-instance, look for atm_nnnn.log*
           for inst in `seq 1 $ninst`; do
             ifilename="`printf "atm_%04d.log.*" $inst`"
             ifile="`ls -t ${ifilename} 2> /dev/null | head -1`"
@@ -62,7 +62,7 @@ else
           errcode=$(( errcode + 1 ))
         else
           for atmfile in ${lfiles}; do
-            dasig="`zgrep '^[ ]*DART run using CAM initial mode$' ${atmfile} 2> /dev/null`"
+            dasig="`zgrep '^[ ]*[*][*][*]DART mode ON[*][*][*]$' ${atmfile} 2> /dev/null`"
             initsig="`zgrep '^[ ]*Initial run$' ${atmfile} 2> /dev/null`"
             if [ $cycle -gt 0 ]; then
               if [ -n "${dasig}" ]; then
@@ -79,11 +79,43 @@ else
               else
                 echo "No initial run signal found for cycle ${cycle}"
               fi
-            elif [ -n "${initsig}" ]; then
+            elif [ -n "${initsig}" -a -z "${dasig}" ]; then
               echo "Bad initial run signal found for cycle ${cycle}"
             fi
           done
         fi
+      fi
+      # Assign the end-of-run initial history file as the next run's ncdata
+      if [ $ninst -eq 1 ]; then
+        lfile="`ls -t DAE.*.cam.i*.nc 2> /dev/null | head -1`"
+        if [ -z "${lfile}" ]; then
+          echo "No inithist file found"
+          errcode=$(( errcode + 1 ))
+        else
+          echo "ncdata = '${rundir}/${lfile}'" >> ${caseroot}/user_nl_cam
+        fi
+      else
+          # Multi-instance, look for cam_nnnn.i*
+          for inst in `seq 1 $ninst`; do
+            ifilename="`printf "DAE.*.cam_%04d.i.*.nc" $inst`"
+            lfile="`ls -t ${ifilename} 2> /dev/null | head -1`"
+            if [ -z "${lfile}" ]; then
+              echo "No inithist file for instance $ninst found"
+              errcode=$(( errcode + 1 ))
+            else
+              iusernlname="`printf "user_nl_cam_%04d" $inst`"
+              echo "ncdata = '${rundir}/${lfile}'" >> ${caseroot}/${iusernlname}
+            fi
+          done
+      fi
+      # Rerun preview-namelists to regenerate atm_in
+      # Output dumped because it should not show up in DA log
+      cd ${caseroot}
+      perr="`./preview_namelists 2>&1`"
+      res=$?
+      if [ $res -ne 0 ]; then
+        echo -e "ERROR: Unable to run preview_namelists\n${perr}"
+        errcode=$(( errcode + 1 ))
       fi
     else
       echo "ERROR: RUNDIR (${rundir}) is not a valid directory"
